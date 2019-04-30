42a98aff7f950cf7c09e7ba94c7b714cb00314f4
Diego <d.rossi025@studenti.unibs.it>
2019-04-30 09:59:12 +0200

    equilibrio commenti

--- src/it/unibs/fp/tamagolem/Equilibrio.java
+++ src/it/unibs/fp/tamagolem/Equilibrio.java
@@ -2,39 +2,38 @@ package it.unibs.fp.tamagolem;
 import java.util.Random;
 public class Equilibrio {
 	
+	//numero di elementi
 	private static final int NUMERO = 5;
 
+	//viene definito il metodo per la creazione della matrice dell'equilibrio
 	public void creaMatrice() {
 	int [][] mat=new int[NUMERO][NUMERO];
 	
 	Random random= new Random();
 	for(int i=0; i<NUMERO; i++) {
-		int somma=0;
-		for(int j=0; j<NUMERO; j++) {
-			if (i<(j-1)) {
-			
-				mat[i][j]= random.nextInt(99)-50+1;
-				if (j==(NUMERO-1)) mat[i][j]=-somma;
-			    		}
-			else if(i==j) mat[i][j]=0;
-			else mat[i][j]=-mat[j][i];
-
-			if (i==j) mat[i][j] = 0;
-			else {
-				if (j==(NUMERO-1)) mat[i][j]=-somma;
+		//la variabile somma memorizza la somma dei valori generati sulla stessa linea
+			int somma=0;
+			for(int j=0; j<NUMERO; j++) {
+				//se il numero della riga è uguale al numero della colonna (elemento uguale), l'interazione è nulla
+				if (i==j) mat[i][j] = 0;
 				else {
-				if (i<j) mat[i][j] = random.nextInt(100)-49;
-				else {
-					if (i>j) mat[i][j] = -mat[j][i];
+					//l'ultimo numero della riga serve a bilanciare, in modo che la somma totale sia 0
+					if (j==(NUMERO-1)) mat[i][j]=-somma;
+					else {
+						//se il numero della riga è minore di quello della colonna (matrice triangolare sup.) si genera un numero casuale
+						if (i<j) mat[i][j] = random.nextInt(100)-49;
+						else {
+							//la matrice triangolare inf. è uguale alla sup. cambiata di segno
+							if (i>j) mat[i][j] = -mat[j][i];
+							}
+						}
 					}
-				}
+				//si aumenta il valore di somma
+				somma=somma+mat[i][j];
 			}
-
-			somma=somma+mat[i][j];
 		}
-	}
 	
-	
+	//stampa della matrice
 	for(int i=0; i<NUMERO; i++) {
 		for(int j=0; j<NUMERO; j++) {
 			System.out.print(mat[i][j] + "\t");
