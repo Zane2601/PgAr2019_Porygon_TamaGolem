fd3efb71b88d486917f17ad307bc4a29e9a03501
Zane2601 <d.zanelli002@studenti.unibs.it>
2019-04-30 12:13:12 +0200

    menu secondario

--- src/it/unibs/fp/tamagolem/Menu.java
+++ src/it/unibs/fp/tamagolem/Menu.java
@@ -10,7 +10,15 @@ public class Menu {
             "Inizia Partita",
             
           };
-	public void mostraMenu() {
+	private static final String TITOLO_MENU_SECONDARIO = "Scegli la difficoltà";
+	private static final String [] VOCI_MENU_SECONDARIO = {
+			"Facile",
+			"Normale",
+			"Difficile"
+	};
+	
+	//viene mostrato il menu per iniziare una partita
+	public void mostraMenuIniziale() {                      
         int scelta;
         MyMenu menu = new MyMenu(TITOLO_MENU, VOCI_MENU_INIZIALE);
        
@@ -30,4 +38,25 @@ public class Menu {
            
         } while (scelta != 0);
     }
+	
+	public void mostraMenuSecondario() {
+		int scelta;
+		MyMenu menu = new MyMenu(TITOLO_MENU_SECONDARIO, VOCI_MENU_SECONDARIO);
+		
+		do {
+			scelta = menu.scegli();
+			switch(scelta) {
+			case 1:
+				System.out.println("Giocherai con 5 elementi e 3 pietre assegnabili ai tuoi TamaGolem");
+				break;
+			case 2:
+				System.out.println("Giocherai con 8 elementi e 4 pietre assegnabili ai tuoi TamaGolem");
+				break;
+			case 3:
+				System.out.println("Giocherai con 10 elementi e 5 pietre assegnabili ai tuoi TamaGolem");
+				break;
+			}
+		}while (scelta !=0);
+		
+	}
 }
