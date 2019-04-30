6e96004d42bc0064c850faf16164809c3430d61d
Zane2601 <d.zanelli002@studenti.unibs.it>
2019-04-30 09:34:51 +0200

    classe menu

---
+++ src/it/unibs/fp/tamagolem/Menu.java
@@ -0,0 +1,8 @@
+package it.unibs.fp.tamagolem;
+import java.lang.*;
+
+public class Menu {
+	
+	System.out.println("Benvenuto nel mondo dei Tamagolem, dove l'equilibrio degli elementi è sempre mutevole, solo i più grandi guerrieri riescono ad adattarsi e a sopravvivere combattendo con i loro fedeli TamaGolem al proprio fianco ");
+
+}
