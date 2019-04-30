1699bcdb0502b965ebf2d7bd8f60fa90ddaadf6f
Zane2601 <d.zanelli002@studenti.unibs.it>
2019-04-30 10:43:38 +0200

    menu

--- src/it/unibs/fp/tamagolem/Menu.java
+++ src/it/unibs/fp/tamagolem/Menu.java
@@ -1,8 +1,37 @@
 package it.unibs.fp.tamagolem;
 import java.lang.*;
+import it.unibs.fp.mylib.MyMenu;;
+
 
 public class Menu {
 	
-	System.out.println("Benvenuto nel mondo dei Tamagolem, dove l'equilibrio degli elementi è sempre mutevole, solo i più grandi guerrieri riescono ad adattarsi e a sopravvivere combattendo con i loro fedeli TamaGolem al proprio fianco ");
-
+	private static final String TITOLO_MENU = "Scegli un'azione";
+	private static final String [] VOCI_MENU_INIZIALE = {
+            "Inizia Partita",
+            "Termina"
+    };
+	public void mostraMenu() {
+        int scelta;
+        MyMenu menu = new MyMenu(TITOLO_MENU, VOCI_MENU_INIZIALE);
+       
+        do {
+   
+            scelta = menu.scegli();
+            switch (scelta) {
+            case 1:
+                
+                
+                break;
+            
+            case 2:
+            	
+                
+                break;
+            
+            default:
+                break;
+            }
+           
+        } while (scelta != 0);
+    }
 }
--- src/it/unibs/fp/tamagolem/TamaGolemMain.java
+++ src/it/unibs/fp/tamagolem/TamaGolemMain.java
@@ -2,10 +2,17 @@ package it.unibs.fp.tamagolem;
 
 public class TamaGolemMain {
 
+	private static final String SALUTO_INIZIALE = "Benvenuto nel mondo dei Tamagolem, dove l'equilibrio degli elementi è sempre mutevole,\n solo i più grandi guerrieri riescono ad adattarsi e a sopravvivere combattendo con i loro fedeli TamaGolem al proprio fianco ";
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Equilibrio e = new Equilibrio();
+		Menu m = new Menu();
+		System.out.println(SALUTO_INIZIALE);
 		e.creaMatrice();
+		m.mostraMenu();
+		
+
   
 	}
 
