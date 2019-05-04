f33b58826a653a977ae55a9d54b4835ce9a1f14e
andrydiurs <a.durso@studenti.unibs.it>
2019-05-04 15:14:26 +0200

    correzione metodo calcoloDanni

--- src/it/unibs/fp/tamagolem/Combattimento.java
+++ src/it/unibs/fp/tamagolem/Combattimento.java
@@ -15,13 +15,13 @@ public class Combattimento {
 	}
 	
 	// vengono passati i tipi delle pietre e trova nella matrice il danno da applicare
-	public int calcoloDanni (String tipo1, String tipo2, int difficolta) {
+	public int calcoloDanni (String tipo1, String tipo2, int difficolta, int [][]matrice) {
 		int danno;
 		
-		if(trovaPosMatrice (tipo1,tipo2,difficolta) < 0) {
-			danno = trovaPosMatrice (tipo1,tipo2,difficolta)*(-1);
-		}else if (trovaPosMatrice (tipo1,tipo2,difficolta) > 0) {
-			danno = trovaPosMatrice (tipo1,tipo2,difficolta);
+		if(trovaPosMatrice (tipo1,tipo2,difficolta,matrice) < 0) {
+			danno = trovaPosMatrice (tipo1,tipo2,difficolta,matrice)*(-1);
+		}else if (trovaPosMatrice (tipo1,tipo2,difficolta,matrice) > 0) {
+			danno = trovaPosMatrice (tipo1,tipo2,difficolta,matrice);
 		}else {
 			danno = 0;
 		}
@@ -30,10 +30,10 @@ public class Combattimento {
 	}
 	
 	// trova la posizione del valore nella matrice
-	public int trovaPosMatrice (String tipo1, String tipo2, int difficolta) {
+	public int trovaPosMatrice (String tipo1, String tipo2, int difficolta,int [][]matrice) {
 		int posTipo1 = -1;
 		int posTipo2 = -1;
-		Equilibrio matrice = new Equilibrio(difficolta);
+		
 		
 		
 		for (int i = 0; i < difficolta; i++) {
@@ -45,13 +45,13 @@ public class Combattimento {
 			}
 		}
 		
-		return matrice.creaEquilibrio()[posTipo1][posTipo2];
+		return matrice[posTipo1][posTipo2];
 	}
 	
 	// serve per decidere quale TamaGolem attaccherà o subirà un attacco
-	public boolean chiAttacca (String tipo1, String tipo2, int difficolta) {
+	public boolean chiAttacca (String tipo1, String tipo2, int difficolta,int [][]matrice) {
 		
-		if (trovaPosMatrice(tipo1, tipo2, difficolta)<0) {
+		if (trovaPosMatrice(tipo1, tipo2, difficolta,matrice)<0) {
 			return true;
 		}else{
 			return false;
