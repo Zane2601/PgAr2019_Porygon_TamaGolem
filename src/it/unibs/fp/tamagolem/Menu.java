677ac54221a3cb74c6c5c64589ea4fdc975cb2d4
Zane2601 <d.zanelli002@studenti.unibs.it>
2019-05-06 17:21:11 +0200

    aggiustamenti vari

--- src/it/unibs/fp/tamagolem/Menu.java
+++ src/it/unibs/fp/tamagolem/Menu.java
@@ -21,12 +21,12 @@ public class Menu {
 	
 	private static final String TITOLO_MENU_COMBATTIMENTO = "Assegna le pietre elementari al tuo tamagolem";
 	private static final String [] VOCI_MENU_COMBATTIMENTO = {
-		"Assegna pietre",
+		
 		"Inizia il combattimento"
 	};
 	
 	//viene mostrato il menu per iniziare una partita
-	public void mostraMenuIniziale() {                      
+	public void mostraMenuIniziale(Combattente comb1, Combattente comb2) {                      
         int scelta;
         MyMenu menu = new MyMenu(TITOLO_MENU, VOCI_MENU_INIZIALE);
        
@@ -35,60 +35,7 @@ public class Menu {
             scelta = menu.scegli();
             switch (scelta) {
             case 1:
-            	Combattente c = new Combattente(null, null);
-            	Combattente c1 = c.creaCombattente();
-                Combattente c2 = c.creaCombattente();
-                //mostraMenuSecondario();
-                int scelta2;
-        		MyMenu menu2 = new MyMenu(TITOLO_MENU_SECONDARIO, VOCI_MENU_SECONDARIO);
-        		int[][] matriceEquilibrio;
-        		do {
-        			scelta2 = menu2.scegli();
-        			switch(scelta2) {
-        			case 1:
-        				System.out.println("Giocherai con 5 elementi e 3 pietre assegnabili ai tuoi TamaGolem");
-        				e.setN(5);
-        				
-        				matriceEquilibrio = e.creaEquilibrio();
-        				//e.stampaMatrice(matriceEquilibrio);
-        				//e.stampaEquilibrioMondo(matriceEquilibrio, 5);
-        				//mostraMenuCombattimento();
-        				int scelta3;
-        				MyMenu menu3 = new MyMenu(TITOLO_MENU_COMBATTIMENTO, VOCI_MENU_COMBATTIMENTO);
-        				do {
-        					scelta3=menu3.scegli();
-        					switch(scelta3) {
-        					case 1:
-        						e.getN();
-        					    System.out.println("questa borsa è comune");
-        						System.out.println(e.creaScortaComune());
-        						break;
-        					case 2:
-        						break;
-        					}
-        					
-        				} while (scelta3!=0);
-        				break;
-        			case 2:
-        				System.out.println("Giocherai con 8 elementi e 4 pietre assegnabili ai tuoi TamaGolem");
-        				e.setN(8);
-        				matriceEquilibrio = e.creaEquilibrio();
-        				//e.stampaMatrice(matriceEquilibrio);
-        				mostraMenuCombattimento();
-        				break;
-        			case 3:
-        				System.out.println("Giocherai con 10 elementi e 5 pietre assegnabili ai tuoi TamaGolem");
-        				e.setN(10);
-        				matriceEquilibrio = e.creaEquilibrio();
-        				//e.stampaMatrice(matriceEquilibrio);
-        				mostraMenuCombattimento();
-        				break;
-        			}
-        		} while (scelta2 !=0);
-                break;
-            
-            
-            default:
+            	mostraMenuSecondario(comb1, comb2);
                 break;
             }
            
@@ -96,12 +43,61 @@ public class Menu {
     }
 	
 	
-	public void mostraMenuSecondario() {
-		
+	public void mostraMenuSecondario(Combattente comb1, Combattente comb2) {
+		int scelta2;
+		MyMenu menu2 = new MyMenu(TITOLO_MENU_SECONDARIO, VOCI_MENU_SECONDARIO);
+		int[][] matriceEquilibrio;
+		do {
+			scelta2 = menu2.scegli();
+			switch(scelta2) {
+			case 1:
+				System.out.println("Giocherai con 5 elementi e 3 pietre assegnabili ai tuoi TamaGolem");
+				e.setN(5);
+				e.creaScortaComune();
+				matriceEquilibrio = e.creaEquilibrio();
+				//e.stampaMatrice(matriceEquilibrio);
+				//e.stampaEquilibrioMondo(matriceEquilibrio, 5);
+				mostraMenuCombattimento(comb1, comb2);
+				
+				break;
+			case 2:
+				System.out.println("Giocherai con 8 elementi e 4 pietre assegnabili ai tuoi TamaGolem");
+				e.setN(8);
+				e.creaScortaComune();
+				matriceEquilibrio = e.creaEquilibrio();
+				//e.stampaMatrice(matriceEquilibrio);
+				mostraMenuCombattimento(comb1, comb2);
+				
+				break;
+			case 3:
+				System.out.println("Giocherai con 10 elementi e 5 pietre assegnabili ai tuoi TamaGolem");
+				e.setN(10);
+				e.creaScortaComune();
+				matriceEquilibrio = e.creaEquilibrio();
+				//e.stampaMatrice(matriceEquilibrio);
+				mostraMenuCombattimento(comb1, comb2);
+				
+				break;
+			}
+		} while (scelta2 !=0);
 		
 	}
 	
-	public void mostraMenuCombattimento() {
-		
+	public void mostraMenuCombattimento(Combattente comb1, Combattente comb2) {
+		int scelta3;
+		MyMenu menu3 = new MyMenu(TITOLO_MENU_COMBATTIMENTO, VOCI_MENU_COMBATTIMENTO);
+		do {
+			scelta3=menu3.scegli();
+			switch(scelta3) {
+			case 1:
+				
+				break;
+			
+			}
+			
+				
+			
+		} while (scelta3!=0);
+		mostraMenuIniziale(comb1, comb2);
 	}
 }
